9f78a1e9894364d9630d69aaa9467d391c784214
ᗜ ‸ ᗜ <su.n@jiangxue.team>
2024-03-16 01:45:37 +0800

    feat: project init

---
+++ wat spring/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wat spring
+//
+//  Created by Akiage on 2024/3/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wat spring/wat_springApp.swift
@@ -0,0 +1,17 @@
+//
+//  wat_springApp.swift
+//  wat spring
+//
+//  Created by Akiage on 2024/3/16.
+//
+
+import SwiftUI
+
+@main
+struct wat_springApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
