263525fc9160861db14c16909eda2dd20509c4bf
ᗜ ‸ ᗜ <su.n@jiangxue.team>
2024-03-21 14:10:43 +0800

    feat: add base widget frame

---
+++ widget/widgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  widgetBundle.swift
+//  widget
+//
+//  Created by Akiage on 2024/3/21.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetBundle: WidgetBundle {
+    var body: some Widget {
+        widget()
+    }
+}
